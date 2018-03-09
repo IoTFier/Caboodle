b4ed5d02d6353a732720c33704ce2b769f60df28
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-03-09 17:14:29 +0100

    sample initial commit

--- Samples/Caboodle.Samples/ViewModel/HomeViewModel.cs
+++ Samples/Caboodle.Samples/ViewModel/HomeViewModel.cs
@@ -17,6 +17,7 @@ namespace Caboodle.Samples.ViewModel
                 new SampleItem("Geocoding", typeof(GeocodingPage), "Easily geocode and reverse geocoding."),
                 new SampleItem("Preferences", typeof(PreferencesPage), "Quickly and easily add persistent preferences."),
                 new SampleItem("SMS", typeof(SMSPage), "Easily send SMS messages."),
+                new SampleItem("Storage Secure", typeof(StorageSecurePage), "Securely store data."),
             };
         }
 
